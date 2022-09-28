285eaf8e80bd2c7f6074fcc45debf20bc86414a3
Jacob deCastro <jacobcdecastro@gmail.com>
2022-09-28 13:22:20 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ const map = new mapboxgl.Map({
   center: config.center,
   zoom: config.zoom,
   transformRequest: transformRequest,
-  cooperativeGestures: true,
+  cooperativeGestures: true
 });
 
 function flyToLocation(currentFeature) {
