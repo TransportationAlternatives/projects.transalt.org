41020689864f0b1dac54c2a2d6e2aa6b9bcbf50d
Jacob deCastro <jacobcdecastro@gmail.com>
2023-12-07 11:19:55 -0500

    Update config-injury.js

--- assets/js/config-injury.js
+++ assets/js/config-injury.js
@@ -5,7 +5,7 @@ const config = {
   style: 'mapbox://styles/mapbox/light-v10',
   accessToken:
     'pk.eyJ1IjoiamFjb2JkZWNhc3RybyIsImEiOiJja3dtZGV5aWsyYjJmMndub2R3cTBuazIyIn0.DaRqJP0ez4gMNtcslbVakQ',
-  CSV: 'https://docs.google.com/spreadsheets/d/1towJSRCkJn_WQWII-iZ-dHLh8YVk5RVwckoA5PjVb4c/gviz/tq?tqx=out:csv&sheet=All%20Qs%20Cleaned',
+  CSV: 'https://docs.google.com/spreadsheets/d/1gNEi8oWb9DIAtBNeupjvU0as7jbgLw2bVK3TTWL0lrI/gviz/tq?tqx=out:csv&sheet=Q3Website',
   center: [-73.92068456351701, 40.73758866530919],
   zoom: 11,
   title: 'See where serious injuries were reported in New York City',
@@ -47,12 +47,13 @@ const config = {
       title: 'Quarter:',
       columnHeader: 'Quarter',
       listItems: [
-        'Quarter 1 - 2022',
-        'Quarter 2 - 2022',
-        'Quarter 3 - 2022',
-        'Quarter 4 - 2022',
-        'Quarter 1 - 2023',
-        'Quarter 2 - 2023',
+        '2022 Q1',
+        '2022 Q2',
+        '2022 Q3',
+        '2022 Q4',
+        '2023 Q1',
+        '2023 Q2',
+        '2023 Q3',
       ],
     },
   ]
