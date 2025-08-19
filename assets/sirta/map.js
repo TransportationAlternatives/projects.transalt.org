e69dc6b6ab90f2d834eff4381d85d6a33458fe23
jack-greenwood-94 <127324334+jack-greenwood-94@users.noreply.github.com>
2025-08-19 13:08:11 -0400

    Update map.js

--- assets/sirta/map.js
+++ assets/sirta/map.js
@@ -40,9 +40,9 @@ var sliderOptions = {
         minProperty: 'Crash_Date',
         maxProperty: 'Crash_Date',
         sliderMin: '2022-01-01',
-        sliderMax: '2025-03-31',
+        sliderMax: '2025-06-30',
         filterMin: '2022-01-01',
-        filterMax: '2025-03-31',
+        filterMax: '2025-06-30',
         propertyType: 'iso8601',
         rangeDescriptionFormat: 'shortDate',
         descriptionPrefix: 'Date:'
