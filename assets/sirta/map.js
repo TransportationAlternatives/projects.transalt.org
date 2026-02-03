0c6f70e70413376aae82065aeb6f6d455cfb4ef2
Jacob deCastro <jacobcdecastro@gmail.com>
2026-02-03 13:58:12 -0500

    Update map.js

--- assets/sirta/map.js
+++ assets/sirta/map.js
@@ -40,9 +40,9 @@ var sliderOptions = {
         minProperty: 'Crash_Date',
         maxProperty: 'Crash_Date',
         sliderMin: '2022-01-01',
-        sliderMax: '2025-12-31',
+        sliderMax: '2025-12-30',
         filterMin: '2022-01-01',
-        filterMax: '2025-12-31',
+        filterMax: '2025-12-30',
         propertyType: 'iso8601',
         rangeDescriptionFormat: 'shortDate',
         descriptionPrefix: 'Date:'
