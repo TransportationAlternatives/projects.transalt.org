12f7511d536e00bf9cdfb1bbb7f56ef66e0bd600
jack-greenwood-94 <127324334+jack-greenwood-94@users.noreply.github.com>
2026-05-13 13:48:12 -0400

    Update map.js

--- assets/vzmap/map.js
+++ assets/vzmap/map.js
@@ -16,9 +16,9 @@ var sliderOptions = {
         minProperty: 'Fatality_Date',
         maxProperty: 'Fatality_Date',
         sliderMin: '2014-01-01',
-        sliderMax: '2026-03-31',
+        sliderMax: '2026-05-04',
         filterMin: '2014-01-01',
-        filterMax: '2026-03-31',
+        filterMax: '2026-05-04',
         propertyType: 'iso8601',
         rangeDescriptionFormat: 'shortDate',
         descriptionPrefix: 'Date:'
