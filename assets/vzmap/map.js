8b4c7de9dabad06da3006614d0ee21331a2a792d
efriedenberg <145372861+efriedenberg@users.noreply.github.com>
2026-07-20 10:52:25 -0400

    Update map.js

--- assets/vzmap/map.js
+++ assets/vzmap/map.js
@@ -16,9 +16,9 @@ var sliderOptions = {
         minProperty: 'Fatality_Date',
         maxProperty: 'Fatality_Date',
         sliderMin: '2014-01-01',
-        sliderMax: '2026-05-17',
+        sliderMax: '2026-06-30',
         filterMin: '2014-01-01',
-        filterMax: '2026-05-17',
+        filterMax: '2026-06-30',
         propertyType: 'iso8601',
         rangeDescriptionFormat: 'shortDate',
         descriptionPrefix: 'Date:'
